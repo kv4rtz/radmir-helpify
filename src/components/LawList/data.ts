293ed532ac7d7d0ec8281dc871ec89ca4036ce5d
kvarcev <0-zh@mail.ru>
2024-02-01 22:49:05 +0400

    fix fraklaws +ng

--- src/components/LawList/data.ts
+++ src/components/LawList/data.ts
@@ -40,10 +40,6 @@ export const constLaws = [
 
 export const laws = [
   {
-    label: 'Закон о национальной гвардии',
-    href: 'https://forum.radmirv.com/index.php?threads/%D0%97%D0%B0%D0%BA%D0%BE%D0%BD-%D0%9E-%D0%9D%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B9-%D0%93%D0%B2%D0%B0%D1%80%D0%B4%D0%B8%D0%B8-%D1%88%D1%82%D0%B0%D1%82%D0%B0-%D0%A1%D0%B0%D0%BD-%D0%90%D0%BD%D0%B4%D1%80%D0%B5%D0%B0%D1%81.110428/',
-  },
-  {
     label: 'Закон о ВП и ЧП',
     href: 'https://forum.radmirv.com/index.php?threads/%D0%97%D0%90%D0%9A%D0%9E%D0%9D-%C2%AB%D0%9E-%D0%92%D0%9E%D0%95%D0%9D%D0%9D%D0%9E%D0%9C-%D0%98-%D0%A7%D0%A0%D0%95%D0%97%D0%92%D0%AB%D0%A7%D0%90%D0%99%D0%9D%D0%9E%D0%9C-%D0%9F%D0%9E%D0%9B%D0%9E%D0%96%D0%95%D0%9D%D0%98%D0%98-%D0%A8%D0%A2%D0%90%D0%A2%D0%90-%D0%A1%D0%90%D0%9D-%D0%90%D0%9D%D0%94%D0%A0%D0%95%D0%90%D0%A1%C2%BB.37281/',
   },
@@ -69,6 +65,10 @@ export const frakLaws = [
   {
     label: 'Закон о полиции',
     href: 'https://forum.radmirv.com/index.php?threads/%D0%97%D0%90%D0%9A%D0%9E%D0%9D-%D0%9E-%D0%93%D0%9E%D0%A1%D0%A3%D0%94%D0%90%D0%A0%D0%A1%D0%A2%D0%92%D0%95%D0%9D%D0%9D%D0%9E%D0%99-%D0%A2%D0%90%D0%99%D0%9D%D0%95-%D0%A8%D0%A2%D0%90%D0%A2%D0%90-%D0%A1%D0%90%D0%9D-%D0%90%D0%9D%D0%94%D0%A0%D0%95%D0%90%D0%A1.177332/',
+  },
+  {
+    label: 'Закон о национальной гвардии',
+    href: 'https://forum.radmirv.com/index.php?threads/%D0%97%D0%B0%D0%BA%D0%BE%D0%BD-%D0%9E-%D0%9D%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B9-%D0%93%D0%B2%D0%B0%D1%80%D0%B4%D0%B8%D0%B8-%D1%88%D1%82%D0%B0%D1%82%D0%B0-%D0%A1%D0%B0%D0%BD-%D0%90%D0%BD%D0%B4%D1%80%D0%B5%D0%B0%D1%81.110428/',
   },
   {
     label: 'Закон об адвокатской деятельности',
